928eae87b9b085297efc67b44586cd07d3a4297f
joy9284 <a0989647245@gmail.com>
2026-04-19 23:20:41 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,9 @@
 const data = {
   "INFP":{
     title:"🌷 INFP 人間仙女",
-    makeup:"溫柔氛圍妝",
+    makeup:"images/products/INFP.png",
     desc:"你屬於比較內斂、感受力很強的人，情緒細膩、容易被氛圍影響。不喜歡太強烈或太張揚的風格，比起吸引所有人，你更在意「剛剛好被看見」。",
     model:"images/models/INFP.png",
-    products:[
-      {img:"images/products/小蜜乳.png", name:"小蜜乳"},
-      {img:"images/products/小奶朵.png", name:"小奶朵"},
-      {img:"images/products/小金桃.png", name:"小金桃"}
-    ]
   }
 };
 
