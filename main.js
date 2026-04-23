6ace222f22fab2427daa04c056c45bc830d0575e
joy9284 <a0989647245@gmail.com>
2026-04-23 15:09:02 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,6 @@ const data = {
     desc:"你屬於比較內斂、感受力很強的人，情緒細膩、容易被氛圍影響。不喜歡太強烈或太張揚的風格，比起吸引所有人，你更在意「剛剛好被看見」。",
     model:"images/models/INFP.png",
   },
-  "ESTJ": {
-    title: "🌷 ESTJ 大地之母", // 你可以之後修改標題
-    makeup: "images/products/ESTJ.png", // 建議換成 ESFJ 對應的圖片路徑
-    desc: "你是溫暖且慷慨的照顧者，天生具有敏銳的觀察力，總能察覺他人的需求並給予最及時的協助。在人群中，你就是最穩定且充滿愛的存在。",
-    model: "images/models/ESTJ.png",
-  },
     "INTJ": {
     title: "INTJ 冷感策士女王", // 你可以之後修改標題
     makeup: "images/products/INTJ.png", // 建議換成 ESFJ 對應的圖片路徑
